e9e33175e734d4aa9f6dbb697f1281c0121ddc4d
thelouisgram <hadesanoye01@gmail.com>
2023-03-17 11:12:19 +0100

    completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <header className="header">
+            <div>
+            <i class="fa-brands fa-react"></i>
+            <h1>ReactFacts</h1>
+            </div>
+            <h3>React Course - Project 1</h3>
+        </header>
+    )
+}
